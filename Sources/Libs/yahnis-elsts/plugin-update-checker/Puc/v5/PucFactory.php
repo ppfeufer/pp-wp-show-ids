09562c59a93505c2d79cb00fdb120f5ff2c426fb
Peter Pfeufer <info@ppfeufer.de>
2024-06-28 12:07:36 +0200

    [ADD] Updater library

---
+++ Sources/Libs/yahnis-elsts/plugin-update-checker/Puc/v5/PucFactory.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace YahnisElsts\PluginUpdateChecker\v5;
+
+if ( !class_exists(PucFactory::class, false) ):
+
+	class PucFactory extends \Ppfeufer\Plugin\PpWpShowIDs\Libs\YahnisElsts\PluginUpdateChecker\v5p4\PucFactory {
+	}
+
+endif;
