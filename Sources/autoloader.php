27b6b38527f7786194f7382ab8c67b7e1f8fbd76
Peter Pfeufer <info@ppfeufer.de>
2024-12-27 12:23:05 +0100

    [CHANGE] Make autoloader namespace-aware

--- Sources/autoloader.php
+++ Sources/autoloader.php
@@ -5,6 +5,11 @@ namespace Ppfeufer\Plugin\PpWpShowIDs;
 use Exception;
 use RuntimeException;
 
+// Register the autoloader.
+// phpcs:disable
+spl_autoload_register(callback: '\Ppfeufer\Plugin\PpWpShowIDs\autoload');
+// phpcs:enable
+
 /**
  * Autoloader for the theme classes and interfaces to be loaded dynamically.
  * This will allow us to include only the files we need when we need them.
@@ -13,24 +18,25 @@ use RuntimeException;
  * @return void
  * @package Ppfeufer\Plugin\PpWpShowIDs
  */
-spl_autoload_register(callback: static function (string $className): void {
+function autoload(string $className): void {
     // Check if the class name starts with the base namespace or includes `Libs' in the path
     if (!str_starts_with(haystack: $className, needle: __NAMESPACE__)
-        || str_contains(haystack: $className, needle: '\Libs')) {
+        || str_contains(haystack: $className, needle: '\Libs')
+    ) {
         return;
     }
 
     // Convert the class name to a relative file path
     $relativeClass = str_replace(
-        [
+        search: [
             __NAMESPACE__ . '\\',
             '\\'
         ],
-        [
+        replace: [
             '',
             DIRECTORY_SEPARATOR
         ],
-        $className
+        subject: $className
     );
 
     // Construct the full file path
@@ -38,18 +44,19 @@ spl_autoload_register(callback: static function (string $className): void {
 
     // Include the file if it exists
     try {
-        if (file_exists($file)) {
+        if (file_exists(filename: $file)) {
             include_once $file;
         } else {
             throw new RuntimeException(
                 sprintf(
-                    'Autoloader error: Class file for %1$s not found at %2$s',
+                    '[%1$s] Autoloader error: Class file for %2$s not found at %3$s',
+                    __NAMESPACE__,
                     $className,
                     $file
                 )
             );
         }
     } catch (Exception $e) {
-        error_log($e->getMessage());
+        error_log(message: $e->getMessage());
     }
-});
+}
